c14aa4e930da86ede29899dbb9321b222a6d9f47
Rashid <rashidstanikzai.net@gmail.com>
2022-08-17 15:16:23 +0500

    database commented

--- hire_a_teacher_rest_api/settings.py
+++ hire_a_teacher_rest_api/settings.py
@@ -99,28 +99,28 @@ WSGI_APPLICATION = "hire_a_teacher_rest_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if "RDS_DB_NAME" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.environ["RDS_DB_NAME"],
-            "USER": os.environ["RDS_USERNAME"],
-            "PASSWORD": os.environ["RDS_PASSWORD"],
-            "HOST": os.environ["RDS_HOSTNAME"],
-            "PORT": os.environ["RDS_PORT"],
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "hireATeacher",
-            "USER": "postgres",
-            "PASSWORD": "bookbook",
-            "HOST": "localhost",
-            "PORT": "5432",
-        }
-    }
+# if "RDS_DB_NAME" in os.environ:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql_psycopg2",
+#             "NAME": os.environ["RDS_DB_NAME"],
+#             "USER": os.environ["RDS_USERNAME"],
+#             "PASSWORD": os.environ["RDS_PASSWORD"],
+#             "HOST": os.environ["RDS_HOSTNAME"],
+#             "PORT": os.environ["RDS_PORT"],
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": "hireATeacher",
+#             "USER": "postgres",
+#             "PASSWORD": "bookbook",
+#             "HOST": "localhost",
+#             "PORT": "5432",
+#         }
+#     }
 
 # DATABASES = {
 #     "default": {
